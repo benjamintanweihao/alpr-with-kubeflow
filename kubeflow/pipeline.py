539d041e531cdd7e898409cf0a75688721025541
Benjamin Tan <benjamintanweihao@gmail.com>
2020-08-17 23:03:50 +0800

    Add MODELS folder. Fix pipeline

--- kubeflow/pipeline.py
+++ kubeflow/pipeline.py
@@ -6,7 +6,7 @@ from kfp import dsl
 from kfp.dsl import PipelineVolume
 from kubernetes.client import V1EnvVar
 
-PROJECT_ROOT = '/workspace'
+PROJECT_ROOT = '/workspace/alpr-with-kubeflow'
 
 
 def add_env_variables(op):
@@ -29,9 +29,12 @@ def git_clone_op(branch_or_sha: str, pvolume: PipelineVolume):
 
     commands = [
         f"git clone https://github.com/benjamintanweihao/alpr-with-kubeflow.git {PROJECT_ROOT}",
-        f"cd {PROJECT_ROOT}/alpr-with-kubeflow",
+        f"cd {PROJECT_ROOT}",
         f"git checkout {branch_or_sha}",
     ]
+
+    for c in commands:
+        print(c)
 
     op = dsl.ContainerOp(
         name='git clone project',
@@ -48,6 +51,9 @@ def git_clone_op(branch_or_sha: str, pvolume: PipelineVolume):
 def convert_to_tfrecords_op(image: str, pvolume: PipelineVolume):
     commands = []
     datasets = ['indian', 'romanian', 'voc']
+
+    commands.append(f'cd {PROJECT_ROOT}')
+
     for d in datasets:
         for split in ['train', 'test']:
             input_dir = os.path.join('DATASETS', d, split, 'annotations')
@@ -63,6 +69,9 @@ def convert_to_tfrecords_op(image: str, pvolume: PipelineVolume):
                 f'python dataprep/scripts/csv2tfrecords.py --split_name={split} --tfrecord_name={d} '
                 f'--label=license-plate --csv_input={csv_input} --output_path TFRECORDS/ --img_path={img_path}'
             )
+
+    for c in commands:
+        print(c)
 
     op = dsl.ContainerOp(
         name='convert to tfrecords',
@@ -80,11 +89,15 @@ def train_and_eval_op(image: str, pvolume: PipelineVolume, model_name: str):
     model_dir = f'LOGS/{model_name}{int(time.time())}'
 
     commands = [
+        f'cd {PROJECT_ROOT}',
         f'python model_main.py --model_dir {model_dir} --pipeline_config_path ../model_configs/{model_name}.config'
     ]
 
+    for c in commands:
+        print(c)
+
     op = dsl.ContainerOp(
-        name='convert to tfrecords',
+        name='train and eval',
         image=image,
         command=['sh'],
         arguments=['-c', ' && '.join(commands)],
@@ -97,10 +110,14 @@ def train_and_eval_op(image: str, pvolume: PipelineVolume, model_name: str):
 
 def export_save_model_op(image: str, pvolume: PipelineVolume, model_name: str):
     commands = [
+        f'cd {PROJECT_ROOT}',
         f'python MODELS/research/object_detection/export_inference_graph.py --input_type image_tensor '
         f'--pipeline_config_path train/model_configs/{model_name}.config '
         f'--trained_checkpoint_prefix LOGS/model.ckpt --output_directory SAVED_MODEL/{model_name}'
     ]
+
+    for c in commands:
+        print(c)
 
     op = dsl.ContainerOp(
         name='export savedmodel',
