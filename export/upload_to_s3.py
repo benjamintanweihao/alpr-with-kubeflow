63510b8df58f299965a3f2291da730023bf3aa01
Benjamin Tan <benjamintanweihao@gmail.com>
2020-08-22 13:48:43 +0800

    Testing out model serving

---
+++ export/upload_to_s3.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import boto3
+from botocore.client import Config
+
+
+def main():
+    parser = argparse.ArgumentParser(description="upload model to S3")
+    parser.add_argument("-d",
+                        "--model_dir",
+                        help="path of the model to be exported",
+                        type=str)
+    parser.add_argument("-b",
+                        "--export_bucket",
+                        help="name of bucket to export to",
+                        type=str)
+    parser.add_argument("-n",
+                        "--model_name",
+                        help="name of the model",
+                        type=str)
+    parser.add_argument("-v",
+                        "--model_version",
+                        help="version of the model",
+                        type=int)
+
+    args = parser.parse_args()
+
+    model_dir = args.model_dir
+    model_name = args.model_name
+    model_version = args.model_version
+    export_bucket = args.export_bucket
+
+    s3 = boto3.client(
+        "s3",
+        endpoint_url="http://minio-service.kubeflow:9000",
+        aws_access_key_id="minio",
+        aws_secret_access_key="minio123",
+        config=Config(signature_version="s3v4"),
+    )
+
+    # Create export bucket if it does not yet exist
+    response = s3.list_buckets()
+    export_bucket_exists = False
+
+    for bucket in response["Buckets"]:
+        if bucket["Name"] == export_bucket:
+            export_bucket_exists = True
+
+    if not export_bucket_exists:
+        s3.create_bucket(ACL="public-read-write", Bucket=export_bucket)
+
+    # Save model files to S3
+    for root, dirs, files in os.walk(model_dir):
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            s3_path = os.path.relpath(local_path, model_dir)
+
+            s3.upload_file(
+                local_path,
+                export_bucket,
+                f"{model_name}/{model_version}/{s3_path}",
+                ExtraArgs={"ACL": "public-read"},
+            )
+
+    response = s3.list_objects(Bucket=export_bucket)
+    print(f"All objects in {export_bucket}:")
+    for file in response["Contents"]:
+        print("{}/{}".format(export_bucket, file["Key"]))
+
+
+if __name__ == '__main__':
+    main()
--- kubeflow/pipeline.py
+++ kubeflow/pipeline.py
@@ -1,7 +1,8 @@
 import os
 
 import kfp
-from kfp import dsl
+from kfp import dsl, components
+from kfp.components import InputPath
 from kfp.dsl import PipelineVolume
 from kubernetes.client import V1EnvVar
 
@@ -10,6 +11,13 @@ PROJECT_ROOT = '/workspace/alpr-with-kubeflow'
 
 def add_env_variables(op):
     op.container.add_env_variable(V1EnvVar(name='TF_FORCE_GPU_ALLOW_GROWTH', value="true"))
+    return op
+
+
+def minio_env_variables(op):
+    op.container.add_env_variable(V1EnvVar(name='MINIO_ACCESS_KEY', value="minio"))
+    op.container.add_env_variable(V1EnvVar(name='MINIO_SECRET_KEY', value="minio123"))
+    op.container.add_env_variable(V1EnvVar(name='MINIO_SSL', value="false"))
     return op
 
 
@@ -164,7 +172,8 @@ def export_saved_model_op(image: str, pvolume: PipelineVolume, model_name: str, 
         f'export PYTHONPATH=$PYTHONPATH:{os.path.join(PROJECT_ROOT, "MODELS")}',
         f'python MODELS/research/object_detection/export_inference_graph.py --input_type image_tensor '
         f'--pipeline_config_path train/model_configs/{model_name}.config '
-        f'--trained_checkpoint_prefix {checkpoint_prefix} --output_directory SAVED_MODEL/{model_name}'
+        f'--trained_checkpoint_prefix {checkpoint_prefix} --output_directory SAVED_MODEL/{model_name}',
+        f'echo {PROJECT_ROOT}/SAVED_MODEL/{model_name} > /workspace/model_dir.txt'
     ]
 
     for c in commands:
@@ -177,7 +186,8 @@ def export_saved_model_op(image: str, pvolume: PipelineVolume, model_name: str, 
         arguments=['-c', ' && '.join(commands)],
         container_kwargs={'image_pull_policy': 'IfNotPresent'},
         pvolumes={"/workspace": pvolume},
-        file_outputs={'saved_model': f'{PROJECT_ROOT}/SAVED_MODEL'}
+        file_outputs={'saved_model': f'{PROJECT_ROOT}/SAVED_MODEL',
+                      'model_dir': f'/workspace/model_dir.txt'}
     )
 
     op = add_env_variables(op)
@@ -185,6 +195,53 @@ def export_saved_model_op(image: str, pvolume: PipelineVolume, model_name: str, 
     op = set_resource_limits(op, cpu_limit='2', memory_limit="4G")
 
     return op
+
+
+def export_model_op(
+        image: str,
+        pvolume: PipelineVolume,
+        model_dir: InputPath(str),
+        export_bucket: str,
+        model_name: str,
+        model_version: int,
+):
+    commands = [
+        f'cd {PROJECT_ROOT}',
+        f'python export/upload_to_s3.py --model_dir={model_dir} '
+        f'--export_bucket={export_bucket} '
+        f'--model_name={model_name} '
+        f'--model_version={model_version} '
+    ]
+
+    for c in commands:
+        print(c)
+
+    op = dsl.ContainerOp(
+        name='upload model to MinIO',
+        image=image,
+        command=['sh'],
+        arguments=['-c', ' && '.join(commands)],
+        container_kwargs={'image_pull_policy': 'IfNotPresent'},
+        pvolumes={"/workspace": pvolume}
+    )
+
+    op = set_resource_request(op, cpu_request='1', memory_request="2G")
+    op = set_resource_limits(op, cpu_limit='2', memory_limit="4G")
+
+    return op
+
+
+def serving_op(export_bucket: str, model_name: str):
+    kfserving_op = components.load_component_from_url(
+        'https://raw.githubusercontent.com/kubeflow/pipelines/master/components/kubeflow/kfserving/component.yaml'
+    )
+
+    return kfserving_op(
+        action="create",
+        default_model_uri=f"s3://{export_bucket}/{model_name}",
+        model_name=model_name,
+        namespace='kubeflow',
+        framework="tensorflow 1.15")
 
 
 @dsl.pipeline(
@@ -195,7 +252,10 @@ def alpr_pipeline(
         image: str = "benjamintanweihao/alpr-kubeflow",
         branch: str = 'master',
         model_name: str = 'ssd_inception_v2_coco',
-        num_train_steps: str = '20000'):
+        num_train_steps: str = '20000',
+        export_bucket: str = 'served_models',
+        model_version: int = 1
+):
     resource_name = 'alpr-pipeline-pvc'
 
     create_pipeline_volume = create_pipeline_volume_op(resource_name=resource_name)
@@ -213,10 +273,20 @@ def alpr_pipeline(
                                           model_name=model_name,
                                           num_train_steps=num_train_steps)
 
-    export_saved_model_op(image=image,
-                          pvolume=training_and_eval.pvolume,
-                          model_name=model_name,
-                          num_train_steps=num_train_steps)
+    export_saved_model = export_saved_model_op(image=image,
+                                               pvolume=training_and_eval.pvolume,
+                                               model_name=model_name,
+                                               num_train_steps=num_train_steps)
+
+    export_model = export_model_op(image=image,
+                                   pvolume=export_saved_model.pvolume,
+                                   model_dir=export_saved_model.outputs['model_dir'],
+                                   export_bucket=export_bucket,
+                                   model_name=model_name,
+                                   model_version=model_version)
+
+    serving_op(export_bucket=export_bucket,
+               model_name=model_name).after(export_model)
 
 
 if __name__ == '__main__':
